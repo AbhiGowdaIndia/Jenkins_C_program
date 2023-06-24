d693727186a6a4f71f2c271013a35cec71440a85
Abhisheka B <44654961+AbhiGowdaIndia@users.noreply.github.com>
2023-06-24 22:16:56 +0530

    Create biggestoftwo.c

---
+++ biggestoftwo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>  
+   
+void biggest_of_two() {  
+    int a, b;  
+    printf("Please Enter Two different values\n");  
+    scanf("%d %d", &a, &b);  
+    
+    if(a > b) 
+    {
+        printf("\n %d is Largest\n", a);          
+    } 
+    else if (b > a)
+    { 
+        printf("\n %d is Largest\n", b);  
+    } 
+    else 
+    {
+	printf("Both are Equal\n");
+    }  
+}
